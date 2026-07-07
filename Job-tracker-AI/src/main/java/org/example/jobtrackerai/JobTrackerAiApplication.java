726ccefee501353a74af5b817c04d05d4904ceaa
Divi osho <diviosho98@gmail.com>
2026-07-07 17:51:10 +0530

    Add initial project setup with Maven and Spring Boot configuration

---
+++ Job-tracker-AI/src/main/java/org/example/jobtrackerai/JobTrackerAiApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobtrackerai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobTrackerAiApplication.class, args);
+    }
+
+}
